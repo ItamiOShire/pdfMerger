079910549e7f82e51ff84cbb181a568b7d2b8e91
Adam <a.niewczas01@gmail.com>
2026-01-14 17:57:38 +0100

    ref: divide app into modules - worker and cloud function

--- worker/src/main/java/com/cloudproject/worker/WorkerApplication.java
+++ worker/src/main/java/com/cloudproject/worker/WorkerApplication.java
@@ -1,13 +1,12 @@
-package com.cloudproject.pdfmerger;
+package com.cloudproject.worker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PdfMergerApplication {
-
+public class WorkerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(PdfMergerApplication.class, args);
+        SpringApplication.run(WorkerApplication.class, args);
+        new PubSubListener().start();
     }
-
 }
